7c8503af12cb29555470ae33fed3a5d288bf0147
Santiago Salazar <s.salazara@uniandes.edu.co>
2018-07-21 00:42:06 -0500

    Cambios para la visualizacion de horarios, falta el client scrapper del banner del usuario

--- FirefoxExt/popup/scripts/main.js
+++ FirefoxExt/popup/scripts/main.js
@@ -1,20 +1,58 @@
-function changeAnimation(components)
+/*
+Función que cambia la animación de las tarjetas SOLAMENTE
+*/
+function changeAnimationCards(cards)
 {
-  components.forEach((comp)=>{
-    let content = document.querySelector(".card");
-    comp.classList.remove("fadeInRight");
-    comp.classList.remove("animated");
+  cards.forEach((card)=>{
+    card.classList.remove("fadeInRight");
+    card.classList.remove("animated");
 
-    comp.classList.add("fadeOutLeft");
-    comp.classList.add("animated");
+    card.classList.add("fadeOutLeft");
+    card.classList.add("animated");
   });
   console.log("ANIMATION CHANGES");
 }
 
+/*
+Función que cambia la animación de los botones SOLAMENTE
+si es true aparece, si es false desaparece.
+*/
+function changeAnimationButtons(buttons, appears)
+{
+  buttons.forEach((but)=>{
+    if(appears)
+    {
+      but.classList.remove("display-none");
+  
+      but.classList.add("zoomIn");
+      but.classList.add("animated");
+    }
+    else
+    {
+      but.classList.remove("zoomIn");
+      but.classList.remove("animated");
+  
+      but.classList.add("zoomOut");
+      but.classList.add("animated");
+      setTimeout(()=>{
+        but.classList.add("display-none");
+      }, 800);
+    }
+
+  });
+  console.log("BUTTON ANIMATION CHANGES");
+}
+
 document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("start")) {
-    var comps = document.querySelectorAll(".card");
-    changeAnimation(comps);
+  if (e.target.classList.contains("start")) 
+  {
+    let cards = document.querySelectorAll(".card");
+    changeAnimationCards(cards);
+
+    //TODO ACA SE EJECUTA EL SCRIPT PARA COGER LAS MATERIAS DE BANNER.
+    /* browser.tabs.executeScript(null, { 
+      file: "scripts/seeBanner.js" 
+    }); */
 
     setTimeout(()=>{
       try {
@@ -24,6 +62,16 @@ document.addEventListener("click", (e) => {
         console.log(e.toString());
         console.log("LOCATION ERROR");
       }
-    }, 700);
+    }, 0);
+  }
+  else if(e.target.classList.contains("accept"))
+  {
+    let butRef = document.querySelectorAll(".refresh");
+    let active = document.querySelectorAll(".accept");
+
+    changeAnimationButtons(active, false)
+    changeAnimationButtons(butRef, true);
+    //TODO AQUI SE HACE LA PETICION AL SCRAPPER Y ESO
+    // O LO QUE SEA QUE SE HAGA AQUI.
   }
 });
